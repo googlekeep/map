90b9ae017431ce095813137c31061bb6ee2118af
googlekeep <info.workingforyou@gmail.com>
2022-10-11 21:12:56 +0530

    reduce

---
+++ reduce.js
@@ -0,0 +1,6 @@
+// map, forEach, filtter, find, reduce
+const numbers = [1, 2, 3, 4, 5,];
+// .reduce(accumulatorFuncton, initital value)
+//  accumulatorFunction use two parameters
+const total = numbers.reduce((previous, current) => previous + current ,0);
+console.log(total);
