4c6af7b990bce4fccd188ca7260e771602e130f4
googlekeep <info.workingforyou@gmail.com>
2022-10-11 19:33:48 +0530

    map with for each

---
+++ forEach.js
@@ -0,0 +1,13 @@
+const friends = ['tom Hanks', 'tom cruises', 'tom bady', 'tom solaiman'];
+
+friends.forEach(friend => {
+    
+})
+
+
+const products = [
+    {id:1, name:'laptop', price: 4000},
+    {id:2, name:'mobile', price: 63000},
+    {id:3, name:'watch', price: 354000},
+    {id:4, name:'tablet', price: 23000},
+];
